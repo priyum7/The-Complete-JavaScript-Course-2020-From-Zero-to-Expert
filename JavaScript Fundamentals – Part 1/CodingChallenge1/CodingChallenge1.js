f426b99cb175b51ab88e78561b9a7c01c4a37d7d
priyum7 <priyumagrawal@gmail.com>
2020-12-05 05:59:22 +0530

    Coding challenge solutions

---
+++ JavaScript Fundamentals – Part 1/CodingChallenge1/CodingChallenge1.js
@@ -0,0 +1,38 @@
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+*/
+
+
+let markMass = window.prompt("Enter Mark's Mass");
+let markHeight = window.prompt("Enter Mark's Height");
+
+let johnMass = window.prompt("Enter John's Mass");
+let johnHeight = window.prompt("Enter John's Height");
+
+let markBMI = markMass / (markHeight ** 2);
+let johnBMI = markMass / (markHeight ** 2);
+
+let markHigherBMI;
+
+if (markBMI > johnBMI) {
+    markHigherBMI = true;
+    console.log("Mark's BMI is greater")
+}
+else {
+    markHigherBMI = false;
+    console.log("John's BMI is greater")
+}
---
+++ JavaScript Fundamentals – Part 1/CodingChallenge2/CodingChallenge2.js
@@ -0,0 +1,26 @@
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+*/
+
+let markMass = window.prompt("Enter Mark's Mass");
+let markHeight = window.prompt("Enter Mark's Height");
+
+let johnMass = window.prompt("Enter John's Mass");
+let johnHeight = window.prompt("Enter John's Height");
+
+let markBMI = markMass / (markHeight ** 2);
+let johnBMI = markMass / (markHeight ** 2);
+
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is greater than John's ${johnBMI}`)
+}
+else {
+    console.log(`John's BMI (${johnBMI}) is greater than Mark's ${markBMI}`)
+}
